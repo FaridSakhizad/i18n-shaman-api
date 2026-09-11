c48b8bdd1bbee07527967208b94377ffba19e8a0
Farid Sakhizad <farid.sakhyzad@gmail.com>
2026-09-10 21:48:50 -0600

    Resurrection | Modernisation | Prettier Disabled

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import eslint from '@eslint/js';
-import prettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -9,7 +8,6 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  prettier,
   {
     files: ['**/*.ts'],
     languageOptions: {
